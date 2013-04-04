86989fcdcd5d3703ef224f8a49d96d1c069032eb
Jannis Grimm <jannis@gje.ch>
2013-04-05 00:31:04 +0200

    Format Code

--- js/ostern/Text.class.js
+++ js/ostern/Text.class.js
@@ -1,5 +1,6 @@
 /**
  * Hufflepuff-Osterprojekt
+ * 
  * @author JANNiS <jannis@huffle-home.de>
  */
 
@@ -7,39 +8,44 @@
  * Klasse für die Texte
  */
 var Text = new Class({
-	/**
+    /**
 	 * Name des Textes
+	 * 
 	 * @type string
 	 */
-	name: null,
-	/**
+    name : null,
+    /**
 	 * Element des Textes
+	 * 
 	 * @type element
 	 */
-	element: null,
-	
-	/**
+    element : null,
+    
+    /**
 	 * Konstruktor, initialisiert den Text
-	 * @param name	string	Name des Textes
-	 * @param id	string	ID des Elements, in dem der Text enthalten ist
+	 * 
+	 * @param name string Name des Textes
+	 * @param id string ID des Elements, in dem der Text enthalten ist
 	 */
-	initialize: function(name, id) {
-		this.name = name;
-		this.element = document.id(id);
-	},
-	
-	/**
-	 * Gibt eine Kopie des Textes zurück
-	 * und ersetzt darin alle {{ }} mit < >
+    initialize : function(name, id)
+    {
+	    this.name = name;
+	    this.element = document.id(id);
+    },
+    
+    /**
+	 * Gibt eine Kopie des Textes zurück und ersetzt darin alle {{ }} mit < >
+	 * 
 	 * @return element Der Text mit ersetzten {{ }}
 	 */
-	getText: function() {
-		var text = this.element.clone();
-		var alt = text.get("html");
-		var neu = alt.replace(/{{(.*)}}/g, "<$1>");
-		text.set("html", neu);
-		text.set("class", "text");
-		text.setStyle("opacity", "0");
-		return text;
-	}
+    getText : function()
+    {
+	    var text = this.element.clone();
+	    var alt = text.get("html");
+	    var neu = alt.replace(/{{(.*)}}/g, "<$1>");
+	    text.set("html", neu);
+	    text.set("class", "text");
+	    text.setStyle("opacity", "0");
+	    return text;
+    }
 });
